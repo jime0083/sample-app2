e8fa493e2fddbcd7b0f5dbddebf37dc7049a5b8a
jime <kuritahajime.business@gmail.com>
2025-02-26 15:29:25 +0900

    vuetify追加 トグル追加

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <div class="d-flex align-center">
+        <span>マイアドレス帳</span>
+      </div>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <SideNav></SideNav>
+    <v-main>
+      
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideNav from './components/SideNav.vue';
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    SideNav
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
